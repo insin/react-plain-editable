3e3cbe4120551101fe1342537018df428f704256
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-02-20 12:15:25 +0000

    v1.0.1

---
+++ CHANGES.md
@@ -0,0 +1,8 @@
+## v1.0.1 / 2015-02-20 / Like a frightened turtle
+
+Fixed shrinkage when empty in FF and IE - now ensures default `innerHTML` is set
+if the `contentEditable` becomes empty.
+
+## v1.0.0 / 2015-02-19 / Ye release
+
+Initial release.
--- dist/react-plain-editable.js
+++ dist/react-plain-editable.js
@@ -1,5 +1,5 @@
 /*!
- * react-plain-editable 1.0.0 - https://github.com/insin/react-plain-editable
+ * react-plain-editable 1.0.1 - https://github.com/insin/react-plain-editable
  * MIT Licensed
  */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.PlainEditable=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -76,11 +76,14 @@ var PlainEditable = React.createClass({displayName: "PlainEditable",
   },
 
   _onInput:function(e) {
-    var html = normaliseContentEditableHTML(e.target.innerHTML)
-    if (html == DEFAULT_CONTENTEDITABLE_HTML) {
-      e.target.innerHTML = html
+    var innerHTML = e.target.innerHTML
+    if (!innerHTML) {
+      e.target.innerHTML = DEFAULT_CONTENTEDITABLE_HTML
     }
-    this.props.onChange(e, returnHTML(html))
+    if (this.props.onChange) {
+      var html = normaliseContentEditableHTML(innerHTML)
+      this.props.onChange(e, returnHTML(html))
+    }
   },
 
   _onKeyDown:function(e) {
@@ -143,9 +146,9 @@ var PlainEditable = React.createClass({displayName: "PlainEditable",
       props, 
       {className: 'PlainEditable' + (className ? ' ' + className : ''), 
       contentEditable: true, 
-      dangerouslySetInnerHTML: {__html: html}, 
+      dangerouslySetInnerHTML: {__html: html || DEFAULT_CONTENTEDITABLE_HTML}, 
       onBlur: onBlur && this._onBlur, 
-      onInput: onChange && this._onInput, 
+      onInput: this._onInput, 
       onFocus: (onFocus || placeholder) && this._onFocus, 
       onKeyDown: (onKeyDown || isIE) && this._onKeyDown, 
       onKeyUp: (onKeyUp || isIE) && this._onKeyUp, 
